01bb5dd3a8e1c454c59493c7847db56bf0f8d1f8
G78405210 <apadron@atlantistecnologia.com>
2024-03-09 21:16:13 +0000

    refactor

--- U6_UniConverter/uni_converter_app/lib/screens/home_screen.dart
+++ U6_UniConverter/uni_converter_app/lib/screens/home_screen.dart
@@ -14,11 +14,11 @@ class HomeScreen extends StatefulWidget {
 class HomeState extends State<HomeScreen> {
    final TextEditingController _valueConverter = TextEditingController();
   String _onzaFuerzaValue = "0.0";
-  String _gramoFuerza = "0.0";
-  String _kilonewton = "0.0";
-  String _libraFuerza = "0.0";
-  String _kilogramoFuerza = "0.0";
-  String _dina = "0.0";
+  String _gramoFuerzaValue = "0.0";
+  String _kilonewtonValue = "0.0";
+  String _libraFuerzaValue = "0.0";
+  String _kilogramoFuerzaValue = "0.0";
+  String _dinaValue = "0.0";
 
   _converterUnits() {    
     double valueConverter = double.parse(_valueConverter.text);
@@ -68,31 +68,31 @@ class HomeState extends State<HomeScreen> {
                      title: const Text("Dina"),
                     subtitle: const Text('1.00E-5N'),
                     leading: const Text("dyn", style: TextStyle(backgroundColor: Colors.lightBlueAccent)),
-                    trailing: Text(_dina)
+                    trailing: Text(_dinaValue)
                   ),
                    ListTile(
                      title: const Text("Kilogramo Fuerza"),
                     subtitle: const Text('9.81 N'),
                     leading: const Text("kgf", style: TextStyle(backgroundColor: Colors.lightBlueAccent)),
-                    trailing: Text(_kilogramoFuerza)
+                    trailing: Text(_kilogramoFuerzaValue)
                   ),
                    ListTile(
                      title: const Text("Libra Fuerza"),
                     subtitle: const Text('4.45 N'),
                     leading: const Text("lbf", style: TextStyle(backgroundColor: Colors.lightBlueAccent)),
-                    trailing: Text(_libraFuerza)
+                    trailing: Text(_libraFuerzaValue)
                   ),
                    ListTile(
                      title: const Text("Kilonewton"),
                     subtitle: const Text('1,000 N'),
                     leading: const Text("kN", style: TextStyle(backgroundColor: Colors.lightBlueAccent)),
-                    trailing: Text(_kilonewton)
+                    trailing: Text(_kilonewtonValue)
                   ),
                    ListTile(
                      title: const Text("Gramo Fuerza"),
                     subtitle: const Text('0.01 N'),
                     leading: const Text("gf", style: TextStyle(backgroundColor: Colors.lightBlueAccent)),
-                    trailing: Text(_gramoFuerza)
+                    trailing: Text(_gramoFuerzaValue)
                   ),
                   ListTile(
                     title: const Text("Onza Fuerza"),
